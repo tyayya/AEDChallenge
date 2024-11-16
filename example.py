3dbb07f3b765886cfb2639c2aecb64c76d623944
alexstedev <4002tsa@gmail.com>
2024-11-16 12:44:05 +0100

    Update example.py

--- example.py
+++ example.py
@@ -1,7 +1,8 @@
-from participant import load_participants
 from rich import print
+
+from participant import load_participants
 
 data_path = "data/datathon_participants.json"
 participants = load_participants(data_path)
 
-print(participants[2])
+print(participants[0])
